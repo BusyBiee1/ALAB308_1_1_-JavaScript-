1a7518dd03c7155031d6c5de1dfce96703646369
busybiee1 <busybiee1@gmail.com>
2024-11-22 11:42:39 -0600

    update

--- index.mjs
+++ index.mjs
@@ -40,11 +40,33 @@ AllNoDivBy5 = AllNoDivBy5 + n2%5;
 AllNoDivBy5 = AllNoDivBy5 + n3%5;
 AllNoDivBy5 = AllNoDivBy5 + n4%5;
 //console.log(AllNoDivBy5);
-  
+isValid == (AllNoDivBy5 == 0);
+//console.log(isValid);
+
 // Check if the first number is larger than the last. Cache the result in a variable.
 let FirstLargerThanLastNo = n1>n4;
 //console.log(FirstLargerThanLastNo);
+//console.log(isValid);
 
+//Accomplish the following arithmetic chain:
+// Subtract the first number from the second number.
+// Multiply the result by the third number.
+// Find the remainder of dividing the result by the fourth number.
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in 
+let subtratFirstFromSecondNo = n2-n1;
+let Multithisby3rdNo = subtratFirstFromSecondNo * n3
+let reminderdivby4thNo = Multithisby3rdNo%4;
+//console.log(reminderdivby4thNo);
+isValid == (reminderdivby4thNo == 0);
+//console.log(isValid);
+
+//Change the way that isOver25 calculates so that we do not need 
+//to use the NOT operator (!) in other logic comparisons. 
+//Rename the variable as appropriate.
+//const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const isOver25_mine = n1 > 25 && n2 > 25 && n3 > 25 && n4 > 25;
+//console.log(isOver25_mine);
+isValid == (reminderdivby4thNo == 0);
 
 ///////////////////////////
 
